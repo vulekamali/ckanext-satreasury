1b91d5253f29c1a5907688274cc7be8d26078516
Greg Kempe <greg@kempe.net>
2017-09-28 17:10:24 +0200

    Hide some facets

--- ckanext/satreasury/plugin.py
+++ ckanext/satreasury/plugin.py
@@ -4,10 +4,24 @@ import ckan.plugins.toolkit as toolkit
 
 class SATreasuryPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IFacets)
 
     # IConfigurer
-
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'satreasury')
+
+    # IFacets
+    def dataset_facets(self, facets_dict, package_type):
+        del facets_dict['organization']
+        del facets_dict['groups']
+        del facets_dict['tags']
+        del facets_dict['license_id']
+        return facets_dict
+
+    def group_facets(self, facets_dict, group_type, package_type):
+        return facets_dict
+
+    def organization_facets(self, facets_dict, organization_type, package_type):
+        return facets_dict
